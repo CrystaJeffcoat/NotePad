848642ee4905b3e4b15797944867775f6d9fbd8d
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-07-19 17:44:38 -0400

    get and post methods added and a function to add notes to database

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const path = require("path");
+const { networkInterfaces } = require("os");
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+
+var allNotes = [];
 
 // create GET http route to /notes to get notes.html file
 app.get("/notes", function(req, res) {
@@ -17,12 +22,51 @@ app.get("/", function(req, res) {
 });
 
 // store and retrieve notes from db.json using fs module
+addToFile({ title: 'mnv', text: 'kjf' });
+function addToFile(newNote) {
+    fs.readFile("db/db.json", function(err, data) {
+        if (data.length > 0) {
+            let notesString = data.toString("utf-8");
+            let notesJSON = JSON.parse(notesString);
+            allNotes = notesJSON;
+            console.log(allNotes);
+            allNotes.push(newNote);
+            console.log(allNotes);
 
-// create GET /api/notes 
-// read from db.json file and return all saved notes as JSON
+            fs.writeFile("db/db.json", JSON.stringify(allNotes), function(err) {
+                if (err) {
+                    console.log(err);
+                };
+            });
+        } else {
+            allNotes.push(newNote);
+            console.log(allNotes)
+            fs.writeFile("db/db.json", JSON.stringify(allNotes), function(err) {
+                if (err) {
+                    console.log(err);
+                };
+            });
+        };
+    });
+};
 
-// create POST 
+// create GET /api/notes
+app.get("/api/notes", function(req, res) {
+    fs.readFile("db/db.json", function(err, allNotes) {
+        //res.writeHead(200);
+        let notesString = allNotes.toString("utf-8");
+        let notesJSON = JSON.parse(notesString);
+        res.json(notesJSON);
+    });
+}); 
 
+// create POST
+app.post("/api/notes", function(req, res) {
+
+    let newNote = req.body;
+    addToFile(newNote);
+
+});
 
 // connect to the server
 app.listen(PORT, function() {
