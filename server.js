cd4aca607d6924ef1cc9a885fd37180c97c00037
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-07-23 20:54:55 -0400

    application deletes and saves notes

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
-
-var allNotes = [];
 
 // create GET http route to /notes to get notes.html file
 app.get("/notes", function(req, res) {
@@ -17,13 +14,16 @@ app.get("/notes", function(req, res) {
 
 // create GET http route to * to get index.html file
 app.get("/", function(req, res) {
-    res.sendFile("public/index.html", {root: __dirname })
+    res.sendFile("public/index.html", {root: __dirname });
 });
 
 // create GET /api/notes
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes/", function(req, res) {
+
     readDataBase(function(err, data) {
+
         res.json(data);
+       
     });
 }); 
 
@@ -31,23 +31,30 @@ app.get("/api/notes", function(req, res) {
 app.post("/api/notes", function(req, res) {
     let newNote = req.body;
     addToFile(newNote);
+    res.end();
 });
 
-app.delete("api/notes/:id", function(id) {
+app.delete("/api/notes/:id", function(req, res) {
+    let chosen = req.params.id;
+
     readDataBase(function(err, data) {
-       
+        newData = data.filter(function(obj) {
+            return obj.id !== parseInt(chosen);
+        });
+        writeDataBase(newData);
     });
-})
+    res.end();
+});
 
 // store and retrieve notes from db.json using fs module
 function addToFile(newNote) {
+    let allNotes = [];
     readDataBase(function(err, data) {
         if (data.length > 0) {
             allNotes = data;
         };
         allNotes.push(newNote);
         writeDataBase(allNotes);
-        console.log(allNotes)
     });
 };
 
@@ -60,12 +67,12 @@ function readDataBase(callback) {
         if (data.length > 0) {
             let notesString = data.toString("utf-8");
             let notesJSON = JSON.parse(notesString);
+            for (i = 0; i < notesJSON.length; i++) {
+                let note = notesJSON[i]
+                note.id = i;
+            };
             callback(null, notesJSON);
         } else callback(null, data);
-        console.log("read file");
-        // for (item in data) {
-        //     console.log(data[item]);
-        //}
     });
 };
 
@@ -75,7 +82,6 @@ function writeDataBase(data) {
         if (err) {
             console.log(err);
         };
-        console.log("wrote to file");
     });
 }
 
